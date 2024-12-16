6ab67dd65a7407785b0cf47ee21166379249d524
fento.013 <fento.013>
2024-12-16 19:27:57 +0100

    tareas a hacer

--- Lenguaje de marcas/clase 4/clase4.js
+++ Lenguaje de marcas/clase 4/clase4.js
@@ -16,3 +16,9 @@ function agregar()
     inNombre.value = ""
     }
 }
+function borrar() {
+    ulNombres.innerHTML = ""
+    lista = []
+//investigar en w3Schools como eliminar un elemento html en javascript, siendo esta splice(), que sirve para agregar, sustituir o borrar
+//comprobar la lsita el nombre introducido dentro del array y eliminarlo dentro del array y el ul
+}
