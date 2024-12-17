4be257ca0e0404a997cc9d0890b73d849a81c9c2
fento.013 <fento.013>
2024-12-17 17:28:36 +0100

    terminar ejercicio

--- Programacion/bidimensionalRepaso/ejercicioSumas/src/App.java
+++ Programacion/bidimensionalRepaso/ejercicioSumas/src/App.java
@@ -5,18 +5,20 @@ public class App {
         int [][] arrayPrueba  = {{1,3},
                                  {5,4},
                                  {7,8}};
+        
+
         for(int i=0;i<arrayPrueba.length;i++){
-        System.out.print(Arrays.toString(sumarArray(arrayPrueba)));   
+            System.out.print(Arrays.toString(sumarArray(arrayPrueba)));   
         }
     }
     public static int[] sumarArray(int[][] array){
         int [] resultado = new int [array.length];
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                resultado[i] += array[i][j];
+        int filas=0;
+        for (int columnas = 0; columnas < array.length; columnas++) {
+            for (int fila = 0; fila < array[fila].length; fila++) {
+                columnas += array[columnas][filas];
             }
         }
         return resultado;
-
     }
 }
