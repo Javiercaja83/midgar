85ee6cdef8e03af50204bd3169c13041efc66dc1
fento.013 <fento.013>
2025-02-13 19:26:37 +0100

    52

--- Programacion/UTT 08/EjemploControlExtepciones/ArithmeticExtepcion/src/App.java
+++ Programacion/UTT 08/EjemploControlExtepciones/ArithmeticExtepcion/src/App.java
@@ -1,4 +1,10 @@
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
 public class App {
 
     public static void main(String[] args) {
@@ -8,10 +14,15 @@ public class App {
             System.out.println("Null pointer exteption"); 
         }
         System.out.println("Fin del programa");
+        try {
+        leerArchivo("nombrearchivo.txt");
+        } catch (Exception e) {
+        System.out.println("Error");
         }
+    }
     
 
-    private static void dividirPorCero() {
+    private static void dividirPorCero() throws ArithmeticException {
         try {
             int a = 10;
             int b = 0;
@@ -31,5 +42,16 @@ public class App {
         System.out.println("NullPointerException..");
     }
 }
-
+    public static void leerArchivo(String nombreArchivo) throws IOException{
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(nombreArchivo));
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }  
+            br.close();
+        } catch (FileNotFoundException e) {
+            nombreArchivo = "rutaDefecto";
+        }
+    }
 }
