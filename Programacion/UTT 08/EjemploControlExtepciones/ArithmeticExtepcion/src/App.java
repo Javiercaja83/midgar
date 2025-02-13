8a4de85a446d268375b355a419af708b6af302bb
fento.013 <fento.013>
2025-02-13 18:59:31 +0100

    522

--- Programacion/UTT 08/EjemploControlExtepciones/ArithmeticExtepcion/src/App.java
+++ Programacion/UTT 08/EjemploControlExtepciones/ArithmeticExtepcion/src/App.java
@@ -3,6 +3,16 @@ public class App {
 
     public static void main(String[] args) {
         try {
+            nullPointerLanza();
+        } catch (ArithmeticException e) {
+            System.out.println("Null pointer exteption"); 
+        }
+        System.out.println("Fin del programa");
+        }
+    
+
+    private static void dividirPorCero() {
+        try {
             int a = 10;
             int b = 0;
             int c = a / b;
@@ -10,18 +20,16 @@ public class App {
         } catch (ArithmeticException e) {
             System.out.println("Error");
         }
-        
-    
+    }
+
+    private static void nullPointerLanza() throws NullPointerException{
         try {
-            String a = null;
-            //null value
-            System.out.println(a.charAt(0));
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerException..");
-        } catch (ArithmeticException e) {
-            System.out.println("Cant divide a numbre by 0");
-        }
-    
-    
+        String a = null;
+        //null value
+        System.out.println(a.charAt(0));
+    } catch (Exception e) {
+        System.out.println("NullPointerException..");
+    }
 }
+
 }
